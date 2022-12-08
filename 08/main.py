921ed879c748331dde3c732629f68728296f47e6
Vaile <valentin.j.schmidt@web.de>
2022-12-08 21:42:53 +0100

    tired

--- 08/main.py
+++ 08/main.py
@@ -1,3 +1,7 @@
-lst = open("\input").readlines()
+lst = open("input").readlines()
 result= []
 for elem in lst:
+    splt = list(elem.rstrip())
+    intsplt = [int(x) for x in splt]
+    result.append(intsplt)
+print(result)
